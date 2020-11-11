2a6efa86f453869be5a9aa56ddd04d1d74b4050d
elosorulo <manustrajman@gmail.com>
2020-11-11 00:08:57 -0300

    Updates

---
+++ precache-manifest.3bb007cb1883520cfdded75a07e81570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a071834dd28c30e99e4ae4db4f0dc6",
+    "url": "/experimento/index.html"
+  },
+  {
+    "revision": "431aad081f53a715ad18",
+    "url": "/experimento/static/css/main.bbb3e13f.chunk.css"
+  },
+  {
+    "revision": "10b058fbd543e5e6134d",
+    "url": "/experimento/static/js/2.5e843f3e.chunk.js"
+  },
+  {
+    "revision": "824e1323383927579d652cf2289254a7",
+    "url": "/experimento/static/js/2.5e843f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431aad081f53a715ad18",
+    "url": "/experimento/static/js/main.f6306c14.chunk.js"
+  },
+  {
+    "revision": "1e16d54bc52fb9cb44c3",
+    "url": "/experimento/static/js/runtime-main.e792161d.js"
+  }
+]);
